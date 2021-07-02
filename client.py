a4fd80f7577991372e227e09d68c542e39d3a216
john <john.chang@nexus-techltd.com>
2021-07-02 10:23:19 +0800

    version-1

--- client.py
+++ client.py
@@ -2,14 +2,16 @@
 Client that sends the file (uploads)
 """
 import socket
-from os import walk
+from os import walk, path
 import re
 import json
 import h5py
 import numpy as np
+import select
+import os
 SEPARATOR = "<SEPARATOR>"
 
-BUFFER_SIZE = 1024 * 4
+BUFFER_SIZE = 8 * 1024
 
 DIRECTORY_PATH = "/home/john/桌面/工作/測試/AI_model_transmission/AI_model_files/z_2021-06-23_z/1"
 
@@ -19,7 +21,7 @@ def get_all_AI_filenames(directory_path):
   for (dirpath, dirnames, filenames) in walk(directory_path):
     
     for file in filenames:
-      if len(re.findall(".h5|.log|.h5last|.csv|.json", file)):
+      if len(re.findall(".h5$|.log$|.h5last$|.csv$|.json$", file)):
         ai_file ={
           "path":dirpath,
           "filename":file
@@ -34,12 +36,34 @@ def open_and_send_h5_file(file_to_be_sent,socket):
     total_bytes = socket.sendfile(file)
     print(total_bytes)
 
+def open_and_send_log_file(file_to_be_sent,socket):
+    BUFFER_SIZE = 8 * 1024
+    with open(file_to_be_sent,'rb') as file:
+        while True:
+          bytes_read = file.read(BUFFER_SIZE)
+          if not bytes_read:
+            break
+          total_bytes = socket.sendall(bytes_read)
+
+def open_and_send_csv_file(file_to_be_sent,socket):
+    BUFFER_SIZE = 8 * 1024
+    with open(file_to_be_sent,'rb') as file:
+        while True:
+          bytes_read = file.read(BUFFER_SIZE)
+          if not bytes_read:
+            break
+          socket.sendall(bytes_read)
+
 def open_and_sned_json_file(file_to_be_sent,socket):
     file = open(file_to_be_sent)
     json_data = json.load(file)
     json_str = json.dumps(json_data)
-    print(json_str)
-    total_bytes = socket.sendall(bytes(json_str,encoding='utf-8'))
+    socket.sendall(bytes(json_str,encoding='utf-8'))
+
+def send_AI_file_list(DIRECTORY_PATH,socket):
+    AI_file_list = get_all_AI_filenames(DIRECTORY_PATH)
+    AI_file_list_json_str = json.dumps(AI_file_list)
+    socket.sendall(AI_file_list_json_str.encode("utf-8"))
 
 def send_file(host, port):
     # create the client socket
@@ -48,11 +72,63 @@ def send_file(host, port):
     s.connect((host, port))
     print("[+] Connected.")
     AI_file_list = get_all_AI_filenames(DIRECTORY_PATH)
-    file_to_be_sent = AI_file_list[0]['path'] + "/" + AI_file_list[0]['filename']
-    open_and_send_h5_file(file_to_be_sent,s)
-    # with open(file_to_be_sent, 'rb') as file:
-    #   total_bytes = s.sendfile(file)
-    #   print(total_bytes)
+    for AI_file in AI_file_list:
+
+      AI_file_path = path.join(AI_file["path"],AI_file["filename"])
+      # print(AI_file_path)
+      if len(re.findall(".h5$",AI_file["filename"])):
+        print("found it! ",AI_file["filename"])
+        open_and_send_h5_file(AI_file_path,s)
+        s.settimeout(10)
+        try:
+          response = s.recv(1024).decode("utf-8")
+          print(response)
+        except:
+          print("why")
+          continue
+      elif len(re.findall(".log$",AI_file["filename"])):
+        print("found it! ",AI_file["filename"])
+        open_and_send_log_file(AI_file_path,s)
+        s.settimeout(10)
+        try:
+          response = s.recv(1024).decode("utf-8")
+          print(response)
+        except:
+          print("why")
+          continue
+      elif len(re.findall(".h5last$",AI_file["filename"])):
+        print("found it! ",AI_file["filename"])
+        open_and_send_h5_file(AI_file_path,s)
+        s.settimeout(10)
+        try:
+          response = s.recv(1024).decode("utf-8")
+          print(response)
+        except:
+          print("why")
+          continue
+      elif len(re.findall(".csv$",AI_file["filename"])):
+        print("found it! ",AI_file["filename"])
+        open_and_send_csv_file(AI_file_path,s)
+        s.settimeout(10)
+        try:
+          response = s.recv(1024).decode("utf-8")
+          print(response)
+        except:
+          print("why")
+          continue
+      elif len(re.findall(".json$",AI_file["filename"])):
+        print("found it! ",AI_file["filename"])
+        open_and_sned_json_file(AI_file_path,s)
+        s.settimeout(10)
+        try:
+          response = s.recv(1024).decode("utf-8")
+          print(response)
+        except:
+          print("why")
+          continue
+      
+      
+
     s.close()
 
 send_file("0.0.0.0",5001)
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import socket
 import os
 import json
 import h5py
+import select
 
 # device's IP address
 SERVER_HOST = "0.0.0.0"
@@ -27,23 +28,84 @@ s.listen(5)
 print(f'[*] Listening as {SERVER_HOST}:{SERVER_PORT }')
 
 
-def save_h5_file(client_socket):
+def save_h5_and_h5last_file(client_socket,file_format):
   CHUNK_SIZE = 8 * 1024
-  chunk = client_socket.recv(CHUNK_SIZE)
-  file = open('test1.h5','wb')
-  file.write(chunk)
-  while chunk:
-    chunk = client_socket.recv(CHUNK_SIZE) 
+  if file_format == '.h5':
+    file = open('test2.h5','wb')
+  elif file_format == '.h5last':
+    file = open('test2.h5last','wb')
+  client_socket.settimeout(1)
+  while True:
+    try:
+      chunk = client_socket.recv(CHUNK_SIZE)
+    except:
+      break
     file.write(chunk)
   file.close()
 
+def save_log_file(client_socket):
+    CHUNK_SIZE = 8 * 1024
+    file = open('test3.log','wb')
+    client_socket.settimeout(1)
+    while True:
+      try:
+        chunk = client_socket.recv(CHUNK_SIZE)
+      except:
+        break
+      file.write(chunk)
+    file.close()
+
+def save_csv_file(client_socket):
+    CHUNK_SIZE = 8 * 1024
+    file = open('test4.csv','wb')
+    client_socket.settimeout(1)
+    while True:
+      try:
+        chunk = client_socket.recv(CHUNK_SIZE)
+      except:
+        break
+      file.write(chunk)
+  
+    file.close()
 
 def save_json_file(client_socket):
-  CHUNK_SIZE = 8 * 1024
-  chunk = client_socket.recv(CHUNK_SIZE).decode('utf-8')
-  chunk = json.loads(chunk)
-  with open("test1.json", "w") as file:
-    json.dump(chunk, file)
+    CHUNK_SIZE = 8 * 1024
+    data = ""
+    client_socket.settimeout(1)
+    while True:
+      try:
+        chunk = client_socket.recv(CHUNK_SIZE).decode('utf-8')
+      except:
+        break
+      data = data + chunk
+    if data:
+      json_data = json.loads(data)
+      with open("test1.json", "w") as file:
+        json.dump(json_data, file)
+
+def receive_AI_file_json_list(client_socket):
+    CHUNK_SIZE = 8 * 1024
+    data = ""
+    client_socket.settimeout(1)
+    while True:
+      try:
+        chunk = client_socket.recv(CHUNK_SIZE).decode("utf-8")
+      except:
+        break
+      data  = data + chunk
+    json_data_list = json.loads(data)
+    return json_data_list
+
+def receive_file_size(client_socket):
+    data = ""
+    client_socket.settimeout(1)
+    while True:
+      try:
+        chunk = client_socket.recv(1024).decode("utf-8")
+        data = data + chunk
+      except:
+        break
+    return data
 
 def receive_file():
   while True:
@@ -51,7 +113,21 @@ def receive_file():
     client_socket, address = s.accept() 
     # if below code is executed, that means the sender is connected
     print(f"[+] {address} is connected.")
-    save_h5_file(client_socket)
+    # Ai_file_list = receive_AI_file_json_list(client_socket)
+    # print(Ai_file_list)
+    save_h5_and_h5last_file(client_socket,".h5")
+    client_socket.sendall("ok".encode("utf-8"))
+    save_log_file(client_socket)
+    client_socket.sendall("ok".encode("utf-8"))
+    save_h5_and_h5last_file(client_socket,".h5last")
+    client_socket.sendall("ok".encode("utf-8"))
+    save_csv_file(client_socket)
+    client_socket.sendall("ok".encode("utf-8"))
+    save_json_file(client_socket)
+    client_socket.sendall("ok".encode("utf-8"))
+
+
+    # client_socket.sendall("success".encode("utf-8"))
     # close the client socket
     client_socket.close()
     # close the server socket
