04844cae88f5f064de857945f1297fba60d4a43b
john <john.chang@nexus-techltd.com>
2021-07-05 13:21:43 +0800

    AI files transmisionn done

--- client.py
+++ client.py
@@ -2,131 +2,23 @@
 Client that sends the file (uploads)
 """
 import socket
-from os import walk, path
-import os
-import re
-import json
-import struct
-import binascii
+from ai_files_transmission import AiFIlesTransmission
+
 
 DIRECTORY_PATH = "/home/john/桌面/工作/測試/AI_model_transmission/AI_model_files/z_2021-06-23_z/1"
 
-def get_all_AI_filenames(directory_path):
-  AI_file_list = []
+def send_AI_files_wrapper(host, port):
   
-  for (dirpath, dirnames, filenames) in walk(directory_path):
-    
-    for file in filenames:
-      if len(re.findall(".h5$|.log$|.h5last$|.csv$|.json$", file)):
-        ai_file ={
-          "path":dirpath,
-          "filename":file
-        }
-
-        AI_file_list.append(ai_file)
-
-  return AI_file_list
-
-def open_and_send_h5_file(file_to_be_sent,socket):
-  with open(file_to_be_sent, 'rb') as file:
-    file_data = file.read()
-    length = struct.pack('>Q', len(file_data))
-    print(int.from_bytes(length,"big"))
-    socket.sendall(length)
-    socket.sendall(file_data)
-    response = socket.recv(1)
-    return response
-
-def open_and_send_log_file(file_to_be_sent,socket):
-    with open(file_to_be_sent,'rb') as file:
-      file_data = file.read()
-      length = struct.pack('>Q', len(file_data))
-      socket.sendall(length)
-      socket.sendall(file_data)
-      response = socket.recv(1)
-      return response
-
-
-def open_and_send_csv_file(file_to_be_sent,socket):
-    with open(file_to_be_sent,'rb') as file:
-      file_data = file.read()
-      length = struct.pack('>Q', len(file_data))
-      socket.sendall(length)
-      socket.sendall(file_data)
-      response = socket.recv(1)
-      return response
-
-
-def open_and_send_json_file(file_to_be_sent,socket):
-    with open(file_to_be_sent, 'rb') as file:
-      file_data = file.read()
-      length = struct.pack('>Q', len(file_data))
-      socket.sendall(length)
-      socket.sendall(file_data)
-      response = socket.recv(1)
-      return response
-
-
-def send_AI_file_list(DIRECTORY_PATH,socket):
-    AI_file_list = get_all_AI_filenames(DIRECTORY_PATH)
-    AI_file_list_with_crc = cal_file_crc(AI_file_list)
-    AI_file_list_json_str = json.dumps(AI_file_list_with_crc)
-    print(AI_file_list_json_str)
-    length = struct.pack('>Q',len(AI_file_list_json_str.encode("utf-8")))
-    socket.sendall(length)
-    socket.sendall(AI_file_list_json_str.encode("utf-8"))
-    response = socket.recv(1)
-    return response
-
-def cal_file_crc(AI_file_list):
-    for AI_file in AI_file_list:
-      file_full_path = os.path.join(AI_file["path"], AI_file["filename"])
-      checksum = crc32(file_full_path)
-      AI_file["checksum"] = checksum
-    return AI_file_list
-
-def send_AI_files(DIRECTORY_PATH,socket):
-    AI_file_list = get_all_AI_filenames(DIRECTORY_PATH)
-    for AI_file in AI_file_list:
-      AI_file_path = path.join(AI_file["path"],AI_file["filename"])
-      print(AI_file_path)
-      if len(re.findall(".h5$",AI_file["filename"])):
-        print("found it! ",AI_file["filename"])
-        response = open_and_send_log_file(AI_file_path,socket)
-      elif len(re.findall(".log$",AI_file["filename"])):
-        print("found it! ",AI_file["filename"])
-        open_and_send_h5_file(AI_file_path,socket)
-      elif len(re.findall(".h5last$",AI_file["filename"])):
-        print("found it! ",AI_file["filename"])
-        open_and_send_h5_file(AI_file_path,socket)
-      elif len(re.findall(".csv$",AI_file["filename"])):
-        print("found it! ",AI_file["filename"])
-        open_and_send_csv_file(AI_file_path,socket)
-      elif len(re.findall(".json$",AI_file["filename"])):
-        print("found it! ",AI_file["filename"])
-        open_and_send_json_file(AI_file_path,socket)
-      if response != b'\x00':
-        return False
-
-def crc32(filename):
-    buf = open(filename,'rb').read()
-    hash = binascii.crc32(buf) & 0xFFFFFFFF
-    return "%08X" % hash
-
-def send_file(host, port):
-    # create the client socket
     s = socket.socket()
     print(f"[+] Connecting to {host}:{port}")
     s.connect((host, port))
     print("[+] Connected.")
-    response = send_AI_file_list(DIRECTORY_PATH,s)
-    if response:
-      send_AI_files(DIRECTORY_PATH,s)
 
-      
-      
+    ai_files_transmission = AiFIlesTransmission()
+    result = ai_files_transmission.start_sending_AI_files(DIRECTORY_PATH,s)
+    print(result)
 
     s.close()
 
-send_file("0.0.0.0",5001)
+send_AI_files_wrapper("0.0.0.0",5001)
 
--- server.py
+++ server.py
@@ -2,160 +2,35 @@
 Server receiver of the file
 """
 import socket
-import os
-import json
-import struct
-import pathlib
-import re
-import binascii
+from ai_files_transmission import AiFIlesTransmission
 
 
 
-# device's IP address
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5001
 
-# create the server socket
-# TCP socket
+
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# bind the socket to our local address
+
 s.bind((SERVER_HOST, SERVER_PORT))
-# enabling our server to accept connections
-# 5 here is the number of unaccepted connections that
-# the system will allow before refusing new connections
+
 s.listen(5)
 print(f'[*] Listening as {SERVER_HOST}:{SERVER_PORT }')
 
-
-def save_h5_and_h5last_file(client_socket,file_format, save_file_path):
-  CHUNK_SIZE = 8 * 1024
-  if file_format == '.h5':
-    file = open(save_file_path,'wb')
-  elif file_format == '.h5last':
-    file = open(save_file_path,'wb')
-  bs = client_socket.recv(8)
-  (length,) = struct.unpack('>Q',bs)
-  print(length)
-  data = b''
-  while len(data) < length:
-    to_read = int(length) - int(len(data))
-    data += client_socket.recv(min(CHUNK_SIZE, to_read))
-    if to_read == 0:
-      break
-  client_socket.sendall(b'\00')
-  file.write(data)
-  print(crc32(save_file_path))
-  file.close()
-
-def save_log_file(client_socket,save_file_path):
-    CHUNK_SIZE = 8 * 1024
-    file = open(save_file_path,'wb')
-    bs = client_socket.recv(8)
-    (length,) = struct.unpack('>Q',bs)
-    data = b''
-    print(length)
-    while len(data) < length:
-      to_read = int(length) - int(len(data))
-      data += client_socket.recv(min(CHUNK_SIZE, to_read))
-      if to_read == 0:
-        break
-    client_socket.sendall(b'\00')
-    file.write(data)
-    file.close()
-
-def save_csv_file(client_socket,save_file_path):
-    CHUNK_SIZE = 8 * 1024
-    file = open(save_file_path,'wb')
-    bs = client_socket.recv(8)
-    (length,) = struct.unpack('>Q',bs)
-    data = b''
-    print(length)
-    while len(data) < length:
-      to_read = int(length) - int(len(data))
-      data += client_socket.recv(min(CHUNK_SIZE, to_read))
-      if to_read == 0:
-        break
-    client_socket.sendall(b'\00')
-    file.write(data)
-    file.close()
-
-def save_json_file(client_socket,save_file_path):
-    CHUNK_SIZE = 8 * 1024
-    file = open(save_file_path,'wb')
-    bs = client_socket.recv(8)
-    (length,) = struct.unpack('>Q',bs)
-    data = b''
-    print(length)
-    while len(data) < length:
-      to_read = int(length) - int(len(data))
-      data += client_socket.recv(min(CHUNK_SIZE, to_read))
-      if to_read == 0:
-        break
-    client_socket.sendall(b'\00')
-    file.write(data)
-
-
-
-def receive_AI_files(client_socket,save_file_path):
-    save_h5_and_h5last_file(client_socket,".h5",save_file_path)
-    save_log_file(client_socket,save_file_path)
-    save_h5_and_h5last_file(client_socket,".h5last",save_file_path)
-    save_csv_file(client_socket,save_file_path)
-    save_json_file(client_socket,save_file_path)
-
-def receive_AI_file_json_list(client_socket):
-    bs = client_socket.recv(8)
-    (length,) = struct.unpack('>Q',bs)
-    data = b''
-    print(length)
-    while len(data) < length:
-      to_read = int(length) - int(len(data))
-      data += client_socket.recv(min(8 * 1024, to_read))
-      if to_read == 0:
-        break
-    AI_file_json_list = json.loads(data.decode("utf-8"))
-    client_socket.sendall(b'\00')
-    return AI_file_json_list
-
-
-def crc32(filename):
-    buf = open(filename,'rb').read()
-    hash = binascii.crc32(buf) & 0xFFFFFFFF
-    return "%08X" % hash
-
-
-def receive_file():
+def receive__AI_files_wrapper():
   while True:
-    # accept connection if there is any
     client_socket, address = s.accept() 
-    # if below code is executed, that means the sender is connected
     print(f"[+] {address} is connected.")
 
     target_dir_path = "/home/john/桌面/工作/測試/AI_model_transmission/dist"
-    AI_file_json_list = receive_AI_file_json_list(client_socket)
-    print(AI_file_json_list)
-    for AI_file in AI_file_json_list:
-      sub_dir = AI_file['path'].split("/")[-1]
-      target_dir_full_path = os.path.join(target_dir_path,sub_dir)
-      path = pathlib.Path(target_dir_full_path)
-      path.mkdir(parents=True, exist_ok=True)
-      save_file_path = os.path.join(target_dir_full_path, AI_file["filename"])
-      if len(re.findall(".h5$",AI_file["filename"])):
-        save_h5_and_h5last_file(client_socket,".h5",save_file_path)
-      elif len(re.findall(".log$",AI_file["filename"])):
-        save_log_file(client_socket,save_file_path)
-      elif len(re.findall(".h5last$",AI_file["filename"])):
-        save_h5_and_h5last_file(client_socket,".h5last",save_file_path)
-      elif len(re.findall(".csv$",AI_file["filename"])):
-        save_csv_file(client_socket,save_file_path)
-      elif len(re.findall(".json$",AI_file["filename"])):
-        save_json_file(client_socket,save_file_path)
+    ai_files_transmission = AiFIlesTransmission()
+    result = ai_files_transmission.receive_AI_files(target_dir_path,client_socket)
+    print(result)
 
-    # client_socket.sendall("success".encode("utf-8"))
-    # close the client socket
     client_socket.close()
-    # close the server socket
+    s.close()
+    
     print(f"[+] {address} has benn disconnected.")
 
-receive_file()
+receive__AI_files_wrapper()
